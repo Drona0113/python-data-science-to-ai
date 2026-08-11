f4ce0a37b58e12b3c0eb3939328b43e17144fcdf
Drona <drona8978@gmail.com>
2026-08-11 10:32:37 +0530

    feat: Implement deposit and withdrawal functionality in Bank class

--- Foundational_Learning/01-Python-Core/Bank_Management/main.py
+++ Foundational_Learning/01-Python-Core/Bank_Management/main.py
@@ -46,6 +46,48 @@ class Bank:
         Bank.data.append(info)
         Bank.__update()
 
+
+    def Deposit_money(self):
+        accnumber=input("enter your account number : ")
+        pin=int(input("enter your pin : "))
+        
+
+        userdata=[i for i in self.data if i["accountNo."]==accnumber and i["pin"]==pin]
+
+        if userdata==False:
+            print("sorry no such account exist")
+        else:
+            amount=int(input("enter amount you want to deposit : "))
+            if amount>10000 or amount<0:
+                print("sorry you can not deposit more than 10000 or less than 0")
+            else:
+                userdata[0]['balance']+=amount
+                self.__update()
+                print("Amount deposited succesfully")
+
+
+    def Withdraw_money(self):
+            accnumber=input("enter your account number : ")
+            pin=int(input("enter your pin : "))
+            
+    
+            userdata=[i for i in self.data if i["accountNo."]==accnumber and i["pin"]==pin]
+    
+            if userdata==False:
+                print("sorry no such account exist")
+            else:
+                amount=int(input("enter amount you want to withdraw : "))
+                if userdata[0]['balance']<amount:
+                    print("sorry you can not withdraw more than your balance")
+                else:
+                    userdata[0]['balance']-=amount
+                    self.__update()
+                    print("Amount withdrawn succesfully")
+    
+
+
+
+
 user=Bank()
 print("press 1 for creating an account")
 print("press 2 for Depositing money in the bank")
@@ -56,3 +98,9 @@ print("press 6 for deleting the account")
 check=int(input("enter your choice : "))
 if check==1:
     user.Create_account()
+
+if check==2:
+    user.Deposit_money()
+
+if check==3:
+    user.Withdraw_money()
